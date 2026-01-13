636f311b0e05d5065f54e3fc5dd93083b9a4c02d
Louise Rigoulot <louise.rigoulot.web@proton.me>
2026-01-13 15:44:44 +0100

    Full TailWind system migration

--- src/js/carroussel.js
+++ src/js/carroussel.js
@@ -1,18 +1,18 @@
 let imgStock = [
   { titre: "Salade Vinegrete", affiche: "hero_img_1.webp" },
-  { titre: "Brochette",        affiche: "hero_img_2.webp" },
+  { titre: "Brochette", affiche: "hero_img_2.webp" },
 ];
 
 let position = 0;
 const img = document.querySelector("#image");
 
 function slide(sens) {
-    position += sens;
+  position += sens;
 
-    if (position > imgStock.length - 1) position = 0;
-    if (position < 0) position = imgStock.length - 1;
+  if (position > imgStock.length - 1) position = 0;
+  if (position < 0) position = imgStock.length - 1;
 
-    // Update image source and alt text
-    img.src = `assets/img/${imgStock[position].affiche}`;
-    img.alt = imgStock[position].titre;
+  // Update image source and alt text
+  img.src = `assets/img/${imgStock[position].affiche}`;
+  img.alt = imgStock[position].titre;
 }
